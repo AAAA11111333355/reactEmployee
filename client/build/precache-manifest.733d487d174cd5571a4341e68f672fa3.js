ac1d809f4d2717c25588feb80e6601ddadf948b2
Anshul <anshul@GEEK>
2020-08-09 01:09:44 +0530

    add build

---
+++ client/build/precache-manifest.733d487d174cd5571a4341e68f672fa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f206db3750edcc08c96d3e5f0944231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73db7a7c86d0bb505247",
+    "url": "/static/js/2.2e3d3be7.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.2e3d3be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913eefdfc11f976ae44a",
+    "url": "/static/js/main.ea44f97c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
